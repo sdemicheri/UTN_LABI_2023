af48a2bc22ead68329e8b65305462a180bb7b9b4
Ailín <115967214+ailinarias@users.noreply.github.com>
2023-08-30 23:51:21 -0300

    Add files via upload

---
+++ ticket.py
@@ -0,0 +1,37 @@
+#Ticket liga de futbol
+
+
+def imprimirarbitros(a1,a2,a3,a4):
+    print("⚽⚽ ARBITROS DESIGNADOS⚽⚽")
+    print(a1)
+    print(a2)
+    print(a3)
+    print(a4)
+    print("⚽⚽⚽⚽⚽⚽⚽⚽⚽⚽⚽⚽⚽⚽\n")
+def canchadiahora(nombre, dia):
+    print("🥅🥅 LUGAR 🥅🥅")
+    print(nombre)
+    print("📆📆 FECHA 📆📆")
+    print(dia)
+
+def impresiondatosdelaliga(telefono, email):
+    print("☎️☎️ Teléfono ☎️☎️")
+    print(telefono)
+    print("📨📨 Correo electrónico 📨📨")
+    print(email)
+
+a1 = input("Ingrese el nombre del arbitro")
+a2 = input("Ingrese el nombre del arbitro")
+a3 = input("Ingrese el nombre del arbitro")
+a4 = input("Ingrese el nombre del arbitro")
+
+nombre = input("Ingrese el nombre de la cancha:\n")
+dia = input("Ingrese el dia en que se va a jugar el partido:\n")
+telefono =input("Ingrese el teléfono de la cancha:\n")
+email = input("Ingrese el correo electrónico de la cancha:\n")
+
+imprimirarbitros(a1,a2,a3,a4)
+canchadiahora (nombre,dia)
+impresiondatosdelaliga(telefono,email)
+
+
