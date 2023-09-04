2d23296a10cb521cab4ab15d2b3475cc4f0b3ae6
Nacho-018 <ignacio.azumendi15@gmail.com>
2023-09-03 22:42:23 -0300

    UpdaterecetaMedica_azumendiC2

--- RecetaMedica.py
+++ RecetaMedica.py
@@ -16,6 +16,16 @@ def medicamento(med, dose, instructions):
 
 def clinica(address):
     print("Por reclamos y consultas acudir a", address, "🔎")
+    guion()
+
+
+def proximoturno(nextTurn, doc, info, tel, name):
+    print("Paciente:", name)
+    print("Turno agendado con el Dr.", doc, "para el", nextTurn)
+    print("Motivo de la consulta:", info)
+    print("En caso de no poder asistir avise con anterioridad \nal número telefónico de referencia")
+    print("\t\t\t\t\t", tel)
+    guion()
 
 
 name = input("Ingrese el nombre completo del paciente: ")
@@ -24,8 +34,12 @@ address = input("Ingrese la dirección de la clínica médica: ")
 med = input("Ingrese el nombre del medicamento a recetar: ")
 dose = input("Ingrese la dosis recomendada del medicamento: ")
 instructions = input("Ingrese las instrucciones de uso del medicamento: ")
+nextTurn = input("Ingrese la fecha y hora del próximo turno del paciente: ")
+doc = input("Ingrese el nombre del médico de cabecera del paciente: ")
+info = input("Ingrese el motivo de la próxima consulta del paciente: ")
+tel = input("Ingrese el número telefónico de la clínica: ")
 
 paciente(name, date)
 medicamento(med, dose, instructions)
 clinica(address)
-guion()
+proximoturno(nextTurn, doc, info, tel, name)
